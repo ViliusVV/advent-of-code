0bf74de6365f8ed2a8061d2464f78a8e2740fd33
Vilius Valinskis <vilius.valinsk@gmail.com>
2023-12-09 21:51:06 +0200

    day 08 part 1

--- src/year2023/day08/part01/Main.kt
+++ src/year2023/day08/part01/Main.kt
@@ -2,7 +2,60 @@ package year2023.day08.part01
 
 import utils.*
 
+data class Instructions (
+    var directions: String = "",
+    var map: MutableMap<String, Pair<String, String>> = mutableMapOf()
+) {
+    fun getDirection(currentStep: Int): Char {
+        return directions[currentStep % directions.length]
+    }
+}
+
 fun main() {
     val input = readInputFile()
-    input.printIt()
+    val instructions = parseInstructions(input)
+
+    instructions.printIt()
+
+    val result = doVisitSequence(instructions)
+    println("Taken steps: $result")
 }
+
+fun doVisitSequence(instructions: Instructions): Int {
+    var currentNode = "AAA"
+    var takenSteps = 0
+
+    while(true) {
+        if(currentNode == "ZZZ") {
+            return takenSteps
+        }
+
+        val nextMap = instructions.map[currentNode]!!
+        val nextDir = instructions.getDirection(takenSteps)
+
+
+        if(nextDir == 'L') {
+            currentNode = nextMap.first
+            println("Going left: $currentNode --> ${nextMap.first}")
+        } else {
+            currentNode = nextMap.second
+            println("Going right: $currentNode --> ${nextMap.second}")
+        }
+
+        takenSteps++
+    }
+}
+
+fun parseInstructions(lines: List<String>): Instructions {
+    val reg = Regex("[A-Z]{3}")
+    val ins = Instructions()
+
+    ins.directions = lines[0]
+    for(i in 1..<lines.size) {
+        val matches = reg.findAll(lines[i]).toList()
+        ins.map[matches[0].value] = Pair(matches[1].value, matches[2].value)
+    }
+
+    return ins
+}
+
