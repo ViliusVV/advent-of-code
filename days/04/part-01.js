820bfcc37d90c1ff0fd0329b406467cee4f4f954
Vilius Valinskis <vilius.valinsk@gmail.com>
2024-12-25 21:37:12 +0200

    browser run update

--- days/04/part-01.js
+++ days/04/part-01.js
@@ -1,1 +1,4 @@
 console.log("Day 04 - Part 01");
+
+
+document.body.innerHTML = "<code>Day 04 - Part 01</code>";
--- main-browser.ts
+++ main-browser.ts
@@ -2,51 +2,75 @@ import { bundle } from "https://deno.land/x/emit/mod.ts";
 import {getDayAndPart, validateScript} from "./lib/deno-file-utils.ts";
 import {debounce} from "./lib/utils.ts";
 
-const OUT_DIR = "./target";
-const BUNDLE_NAME = "main.bundle.js";
-const DATA_PATH = "/data.txt";
-const BUNDLE_PATH = `${OUT_DIR}/${BUNDLE_NAME}`;
-const INDEX_PATH = `${OUT_DIR}/index.html`
+const DATA_NAME = "data.txt";
+
+const TARGET_DIR = "./target";
+const INDEX_TEMPLATE_PATH = `${TARGET_DIR}/index.template.html`;
+const GENERATED_DIR = `${TARGET_DIR}/generated`;
+const INDEX_PATH = `${GENERATED_DIR}/index.html`
 
 function getIndexResponse() {
     const indexContent = Deno.readTextFileSync(INDEX_PATH);
     return new Response(indexContent, { headers: { "content-type": "text/html" }});
 }
 
-function getCodeResponse() {
-    const code = Deno.readTextFileSync(BUNDLE_PATH);
+function getCodeResponse(path: string) {
+    const processedPath = path.replace("/", "");
+    const bundlePath = getBundlePath(processedPath);
+    const code = Deno.readTextFileSync(bundlePath);
     return new Response(code, { headers: { "content-type": "application/javascript" }});
 }
 
 function getDataResponse(ctx: AppContext) {
-    const dataContent = Deno.readTextFileSync(`${ctx.dayDir}/${DATA_PATH}`);
+    const dataContent = Deno.readTextFileSync(`${ctx.dayDir}/${DATA_NAME}`);
     return new Response(dataContent, { headers: { "content-type": "text/plain" }});
 }
 
-async function compileCode(scriptFile: string) {
-    const compiled = await bundle(scriptFile);
+function createTemplatedIndex(bundleName: string) {
+    const indexTemplate = Deno.readTextFileSync(INDEX_TEMPLATE_PATH)
+    const processed = indexTemplate.replace("{{BUNDLE_NAME}}", bundleName);
+    Deno.writeTextFileSync(INDEX_PATH, processed);
+    console.log("Updated index.html");
+    return processed;
+}
+
+function getBundlePath(bundleName: string) {
+    return `${GENERATED_DIR}/${bundleName}`;
+}
+
+function getBundleName(ctx: AppContext) {
+    return `bundle-${ctx.day}-${ctx.part}.${ctx.compileCnt}.js`;
+}
+
+async function compileCode(ctx: AppContext) {
+    const compiled = await bundle(ctx.scriptFile);
     const code = compiled.code;
-    Deno.writeTextFileSync(BUNDLE_PATH, code);
-    console.log("Compiled", scriptFile);
+
+    ctx.bundleName = getBundleName(ctx);
+    const bundlePath = getBundlePath(ctx.bundleName);
+
+    Deno.writeTextFileSync(bundlePath , code);
+    createTemplatedIndex(ctx.bundleName );
+    console.log(`Compiled ${ctx.scriptFile} to ${bundlePath}`);
     return code;
 }
 
 function handleRequestRoute(req: Request, ctx: AppContext): Response {
     const url = new URL(req.url);
     const path = url.pathname;
-    console.log(path);
+    console.log("GET:", path);
 
 
     // Index/entrypoint
-    if(path === "/") {
+    if(path === "/" || path === "/index.html") {
         return getIndexResponse();
     }
     // Handle JS bundle requests
-    else if(path === `/${BUNDLE_NAME}`) {
-        return getCodeResponse();
+    else if(path.endsWith(".js")) {
+        return getCodeResponse(path);
     }
     // Handle data requests
-    else if(path === DATA_PATH) {
+    else if(path === `/${DATA_NAME}`) {
         return getDataResponse(ctx)
     }
     // Not Found
@@ -55,29 +79,9 @@ function handleRequestRoute(req: Request, ctx: AppContext): Response {
     }
 }
 
-type AppContext = {
-    code: string;
-    scriptFile: string;
-    day: string;
-    dayDir: string;
-}
-
-if(import.meta.main) {
-    console.log("AoC Entry Point");
-
-    const scriptFile = Deno.args[0];
-    const {day} = getDayAndPart(Deno.args[0]);
-    validateScript(scriptFile);
-
-    const ctx : AppContext = {
-        code: await compileCode(scriptFile),
-        day: day,
-        dayDir: `days/${day}`,
-        scriptFile: scriptFile
-    }
-
+async function watchAndCompileFiles(ctx: AppContext) {
     const debouncedCompile = debounce(async () => {
-        ctx.code =  await compileCode(scriptFile)
+        ctx.code =  await compileCode(ctx)
     }, 250);
 
     const watcher = Deno.watchFs(ctx.dayDir);
@@ -87,6 +91,37 @@ if(import.meta.main) {
             debouncedCompile();
         }
     }
+}
+
+type AppContext = {
+    code?: string;
+    scriptFile: string;
+    day: string;
+    part: string;
+    dayDir: string;
+    compileCnt: number;
+    bundleName?: string;
+}
+
+if(import.meta.main) {
+    console.log("AoC Entry Point");
+
+    const scriptFile = Deno.args[0];
+    const {day, part} = getDayAndPart(Deno.args[0]);
+    validateScript(scriptFile);
+
+    const ctx : AppContext = {
+        part: part,
+        day: day,
+        dayDir: `days/${day}`,
+        scriptFile: scriptFile,
+        compileCnt: 0
+    }
+
+    ctx.code = await compileCode(ctx);
+
+    // noinspection JSIgnoredPromiseFromCall
+    watchAndCompileFiles(ctx)
 
     Deno.serve(
         { port: 8000 },
